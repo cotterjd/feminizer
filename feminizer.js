da88e1ceae12fd75d611fc214c9f6902091e97ec
Jordan Cotter <jrdn.cttr@gmail.com>
2017-05-25 13:21:15 -0500

    feminizer - makes male pronouns female pronouns

---
+++ feminizer.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ his /gi, " her ")
+document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ him /gi, " her ")
+document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ he /gi, " she ")
+document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ he'll /gi, " she'll ")
+document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ he's /gi, " she's ")
+
+console.log("this page has been feminized")
+

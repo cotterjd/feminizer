51e16c8d21cc7aa853099ab74a25a50c8c0480fb
Jordan Cotter <jrdn.cttr@gmail.com>
2017-07-08 21:20:29 -0500

    refactored and added replacements of pronouns at the end of sentences.

--- feminizer.js
+++ feminizer.js
@@ -1,8 +1,17 @@
-document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ his /gi, " her ")
-document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ him /gi, " her ")
-document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ he /gi, " she ")
-document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ he'll /gi, " she'll ")
-document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(/ he's /gi, " she's ")
+function feminize (oldtext, newtext) {
+	const nil = null 
+	, regex1 = new RegExp(' '+oldtext+' ', 'gi')
+	, regex2 = new RegExp(' '+oldtext+'. ', 'gi')
+	;
+
+	document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(regex1, ' '+newtext+' ')
+	document.querySelectorAll('.bodyText')[0].innerHTML = document.querySelectorAll('.bodyText')[0].innerHTML.replace(regex2, ' '+newtext+'. ')
+}
+
+feminize('his', 'hers')
+feminize('him', 'her')
+feminize('he', 'she')
+feminize("he'll", "she'll")
+feminize("he's", "she's")
 
 console.log("this page has been feminized")
-
